ce9efe9595f3e0e6d8143acb75f6e443fac8ec02
binrongZheng <binrongzheng@gmail.com>
2018-06-27 19:38:00 +0200

    prototype ok

---
+++ Color Switch/Assets/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	public float speed = 100f;
+	private int direection=1;
+
+	void Start(){
+		direection=Random.Range(0,1);
+		if(direection==0)
+			speed=Random.Range(100,150);
+		else
+			speed=Random.Range(-100,-150);
+	}
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0f,0f,speed*Time.deltaTime);
+	}
+}
---
+++ Color Switch/Assets/Script/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour {
+
+	public float JumpForce = 10f;
+
+	public Rigidbody2D rb;
+	public SpriteRenderer sr;
+
+	public string currentColor;
+
+	public Color colorCyan;
+	public Color colorYellow;
+	public Color colorPink;
+	public Color colorPurple;
+
+	void Start(){
+		
+		setRenderColor();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump")||Input.GetMouseButtonDown(0)){
+			rb.velocity=Vector2.up*JumpForce;
+		}
+	}
+
+	void setRenderColor(){
+		int index=Random.Range(0,4);
+
+		switch (index){
+			case 0:
+				currentColor="Cyan";
+				sr.color=colorCyan;
+				break;
+			case 1:
+				currentColor="Yellow";
+				sr.color=colorYellow;
+				break;
+			case 2:
+				currentColor="Pink";
+				sr.color=colorPink;
+				break;
+			case 3:
+				currentColor="Purple";
+				sr.color=colorPurple;
+				break;
+		}
+			
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if(col.tag=="colorChanger"){
+			setRenderColor();
+			Destroy(col.gameObject);
+			return;
+		}
+		if(col.tag!=currentColor){
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		}
+
+	}
+}
